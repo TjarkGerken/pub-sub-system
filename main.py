252dc0f53d7b3db6bb13e2be37a4ce93d1685684
Niklas Seither <niklas.seither@pwc.com>
2024-07-28 12:48:17 +0200

    different stuff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from classes.messageBroker import MessageBroker
+from classes.message_broker import MessageBroker
 
 from classes.sensor import Sensor
 from classes.subscriber import Subscriber
@@ -7,26 +7,26 @@ from classes.subscriber import Subscriber
 def main():
     mb = MessageBroker()
 
-    for i in range(1, 24):
-        sensor = Sensor(sensor_port=5100 + i, sensor_type="U" if i % 2 == 0 else "S",
-                        location="BRM" if i % 2 == 0 else "MHN")
+    for i in range(1, 2):
+        #sensor = Sensor(sensor_port=5100 + i, sensor_type="U" if i % 2 == 0 else "S",
+         #               location="BRM" if i % 2 == 0 else "MHN")
 
-        # subscriber = Subscriber(subscriber_port=6200 + i,
-        #                       subscriber_type="B" if i % 3 == 0 else "U" if i % 3 == 1 else "S")
+        subscriber = Subscriber(subscriber_port=6200 + i+1,
+                               subscriber_type="B") #  if i % 3 == 0 else "U" if i % 3 == 1 else "S"
 
 
-def test_case_1():
-    mb = MessageBroker()
-    s1 = Sensor(sensor_port=5100, sensor_type="U", location="BRM")
-    s1 = Sensor(sensor_port=5102, sensor_type="U", location="BRM")
-    s1 = Sensor(sensor_port=5105, sensor_type="U", location="BRM")
-    # sb = Subscriber(subscriber_port=6201, subscriber_type="B")
+#def test_case_1():
+#   mb = MessageBroker()
+#  s1 = Sensor(sensor_port=5100, sensor_type="U", location="BRM")
+# # s1 = Sensor(sensor_port=5102, sensor_type="U", location="BRM")
+# s1 = Sensor(sensor_port=5105, sensor_type="U", location="BRM")
+#sb = Subscriber(subscriber_port=6201, subscriber_type="B")
 
 
 if __name__ == '__main__':
-    main()
+     main()
 
-    # test_case_1()
+    #test_case_1()
 
 # TODO: https://docs.python.org/3/howto/logging.html
 # TODO: Three Way Handshake
