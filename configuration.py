625b47f2d757171aae828c0bc6c41d829130284c
Niklas Seither <niklas.seither@pwc.com>
2024-07-28 12:47:01 +0200

    Added docstrings

--- configuration.py
+++ configuration.py
@@ -1,6 +1,24 @@
-# Definition of changeable configurations
+"""
+Definition of changeable configurations
 
+Configuration options:
+    - Communication Protocol: Settings related to message sending and retry mechanisms
+    - Logging: Settings for logging severity levels and message formatting.
+    - Client Settings: Settings related to sensors (publishers) and subscribers
+"""
+
+# Communication Protocol
+## Maximum duration to wait for a message to be sent successfully
 RETRY_DURATION_IN_SECONDS = 300
-LOGGING_LEVEL = "INFO"
-MAX_SENSOR_INTERVAL_IN_SECONDS = 1
+## Duration to wait before retrying to send a failed request in secnods
 SECONDS_BETWEEN_RETRIES = 2
+
+# Logging
+## Severity level (choose from 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')
+LOGGING_LEVEL = "INFO"
+## Padding size for log messages
+LOGGING_PADDING = 65
+
+# Client Settings
+## Maximum interval between sensor readings in seconds
+MAX_SENSOR_INTERVAL_IN_SECONDS = 1
--- utils/log_formatter.py
+++ utils/log_formatter.py
@@ -1,19 +1,55 @@
 import logging
 
+from configuration import LOGGING_PADDING
+
 
 class BaseFormatter(logging.Formatter):
-    def __init__(self, format):
-        super().__init__(format)
+    """
+    Custom logging formatter that extends the standard logging.Formatter class
+
+    Attributes:
+        __num_digits : int
+            The number of digits used for padding the custom group string
+    """
+    def __init__(self, fmt):
+        """
+        Initializes the BaseFormatter with a specified format
+
+        :param fmt: The logging format string.
+        :return: None
+        """
+        super().__init__(fmt)
+        self._num_digits = LOGGING_PADDING
 
     def format(self, record):
-        record.custom_group = f"{record.filename}:{record.lineno} - {record.funcName}()".ljust(45)
+        """
+        Formats the log record as text with additional information
+
+        :param record: The log record to be formatted
+        :return: The formatted log record
+        """
+        record.custom_group = f"{record.filename}:{record.lineno} - {record.funcName}()".ljust(self._num_digits)
         return super().format(record)
 
 
 class ColorFormatter(BaseFormatter):
-    def __init__(self, format):
-        super().__init__(format)
-        grey = "\x1b[38;20m"
+    """
+    Custom logging formatter that extends the BaseFormatter class and adds color to the log messages based on their
+    severity level
+
+    Attributes:
+        formats : dict
+            A dictionary that maps the logging levels to their corresponding color codes
+    """
+    def __init__(self, fmt):
+        """
+        Initializes the ColorFormatter with a specified format and sets up color codes for different log levels
+
+        :param fmt: The logging format string
+        :return: None
+        """
+        super().__init__(fmt)
+        grey = "\x1b[0;37m"
         yellow = "\x1b[33;20m"
         red = "\x1b[31;20m"
         bold_red = "\x1b[31;1m"
@@ -21,15 +57,22 @@ class ColorFormatter(BaseFormatter):
         reset = "\x1b[0m"
 
         self.formats = {
-            logging.DEBUG: grey + format + reset,
-            logging.INFO: green + format + reset,
-            logging.WARNING: yellow + format + reset,
-            logging.ERROR: red + format + reset,
-            logging.CRITICAL: bold_red + format + reset
+            logging.DEBUG: grey + fmt + reset,
+            logging.INFO: green + fmt + reset,
+            logging.WARNING: yellow + fmt + reset,
+            logging.ERROR: red + fmt + reset,
+            logging.CRITICAL: bold_red + fmt + reset
         }
 
     def format(self, record):
-        record.custom_group = f"{record.filename}:{record.lineno} - {record.funcName}()".ljust(45)
+        """
+        Formats the specified record as text, applying color codes based on the log level
+
+        :param record: The log record to be formatted
+        :return: The formatted log record with color codes applied
+        """
+        record.custom_group = f"{record.filename}:{record.lineno} - {record.funcName}()".ljust(self._num_digits)
         log_fmt = self.formats.get(record.levelno, self._fmt)
         formatter = logging.Formatter(log_fmt)
+
         return formatter.format(record)
